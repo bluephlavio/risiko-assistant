27ab2f112ac7f3bf77dc93cbc051b80345c68b73
bluephlavio <flavio.grandin@gmail.com>
2019-04-18 20:08:03 +0200

    project setup with firebase cli

--- functions/src/index.ts
+++
@@ -1,6 +0,0 @@
-import * as functions from 'firebase-functions';
-
-export const diceRoll = functions.https.onRequest((request, response) => {
-  const a = Math.ceil(6 * Math.random());
- response.send(`Ho lanciato e ottenuto ${a}`);
-});
